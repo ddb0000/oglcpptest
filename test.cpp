b9df66589e0e519687fbe3a43c1b18ba405e7b99
ddb0000 <dbarcelloz@gmail.com>
2024-04-17 11:28:46 -0300

    coconut

--- test.cpp
+++ test.cpp
@@ -1,13 +1,43 @@
 #include <GLFW/glfw3.h>
 #include <iostream>
 
-// Define a camera struct or class (this is a simple placeholder)
 struct Camera {
     float x, y, z;
     Camera() : x(0), y(0), z(0) {}
 };
 
+const char* vertexShaderSource = "#version 330 core\n"
+    "layout (location = 0) in vec3 aPos;\n"
+    "void main()\n"
+    "{\n"
+    "   gl_Position = vec4(aPos, 1.0);\n"
+    "}\0";
+
+const char* fragmentShaderSource = "#version 330 core\n"
+    "out vec4 FragColor;\n"
+    "void main()\n"
+    "{\n"
+    "   FragColor = vec4(0.5, 0.5, 0.5, 1.0);\n"
+    "}\n\0";
+
 int main() {
+
+    unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+    glCompileShader(vertexShader);
+
+    unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+    glCompileShader(fragmentShader);
+
+    unsigned int shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+    
     if (!glfwInit()) {
         std::cerr << "failed:glfw\n";
         return -1;
@@ -24,25 +54,28 @@ int main() {
 
     Camera camera;
 
-    // this value will control the speed of the movement
     const float movementSpeed = 0.05f;
 
     while (!glfwWindowShouldClose(window)) {
-        // Poll for and process events
         glfwPollEvents();
 
-        // Handle input
         if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS) camera.z -= movementSpeed;
         if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS) camera.z += movementSpeed;
         if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS) camera.x -= movementSpeed;
         if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS) camera.x += movementSpeed;
 
-        // update view matrix with camera's new position
-        // and then use that view matrix for rendering
-
-        // Swap front and back buffers
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        glUseProgram(shaderProgram);
+        glBindVertexArray(VAO); 
+        glDrawArrays(GL_TRIANGLES, 0, 6);
+        glBindVertexArray(0);
+        
         glfwSwapBuffers(window);
     }
+
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
+    glDeleteProgram(shaderProgram);
 
     glfwDestroyWindow(window);
     glfwTerminate();
